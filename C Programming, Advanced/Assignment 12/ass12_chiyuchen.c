9f6daeebd6eab83d8cf023231e65ab67c431f18a
Chi Yu Chen <cjworld1208@gmail.com>
2018-11-08 13:44:04 -0800

    assignment 12 for c adv

--- C Programming, Advanced/Assignment 12/ass12_chiyuchen.c
+++ C Programming, Advanced/Assignment 12/ass12_chiyuchen.c
@@ -16,7 +16,7 @@
 /*                                                            */
 /*       File Name: ass12_chiyuchen.c                         */
 /*                                                            */
-/*       Date: 10/02/2018                                     */
+/*       Date: 11/08/2018                                     */
 /*                                                            */
 /*       Objective: Implement all sorting methods             */
 /*                                                            */
@@ -27,21 +27,42 @@
 /* preprocessor directives */
 #include<stdio.h>
 #include<stdlib.h>
+#define MAXSIZE 100
+#define EMPTYSTACK -1
+#define TRUE 1
+#define FALSE 0
 
 /* global declarations */
 
 /* function prototypes */
+void printarray(int [], int, FILE *);
+void swap(int *, int *);
+void bubble_sort(int [], int);
+void insertion_sort(int [], int);
+void selection_sort(int [], int);
+void shell_sort(int [], int);
 
-void printarray(int array[], int length)
+int partition(int [], int, int);
+void quick_sort(int [], int, int, int);
+void heapify(int [], int, int);
+void heap_sort(int [], int);
+void merge(int [], int, int, int);
+void merge_sort(int [], int, int, int);
+
+void duplicate(int [], int [], int);
+
+/* Print out the array. */
+void printarray(int array[], int length, FILE *fout)
 {
     int i, j, tmp;
     for (i = 0; i < length; i++)
     {
-        printf("%d, ", array[i]);
+        fprintf(fout, "%d, ", array[i]);
     }
-    printf("\n");
+    fprintf(fout, "\n");
 }
 
+/* Swap the values of two integer. */
 void swap(int *a, int *b)
 {
     int tmp;
@@ -50,7 +71,8 @@ void swap(int *a, int *b)
     *a = tmp;
 }
 
-void bubblesort(int array[], int length)
+/* sort an array of integers by bubble sort. */
+void bubble_sort(int array[], int length)
 {
     int i, j, tmp;
     for (i = 0; i < length - 1; i++)
@@ -62,411 +84,351 @@ void bubblesort(int array[], int length)
                 swap(&array[j], &array[j + 1]);
             }
         }
-        printarray(array, length);
+        // printarray(array, length);
     }
 }
 
-void insertsort(int array[], int length)
+/* sort an array of integers by insertion sort. */
+void insertion_sort(int array[], int length)
 {
     int i, j;
     for (i = 1; i < length; i++)
     {
-        for (j = i; j > 0; j--)
+        j = i;
+        while (j > 0 && array[j - 1] > array[j])
         {
-            if (array[j-1] > array[j])
-            {
-                swap(&array[j-1], &array[j]);
-            }
+            swap(&array[j - 1], &array[j]);
+            j--;
         }
-        printarray(array, length);
+        // printarray(array, length);
     }
 }
 
-void selectionsort(int array[], int length)
+/* sort an array of integers by selection sort. */
+void selection_sort(int array[], int length)
 {
-    int i, j, val, idx;
+    int i, j, min, min_idx;
     for (i = 0; i < length - 1; i++)
     {
-        val = array[i];
-        for (j = i; j < length; j++)
+        min_idx = i;
+        for (j = i + 1; j < length; j++)
         {
-            if (val > array[j])
+            if (array[min_idx] > array[j])
             {
-                val = array[j];
-                idx = j;
+                min_idx = j;
             }
         }
-        swap(&array[i], &array[idx]);
-        printarray(array, length);
+        swap(&array[i], &array[min_idx]);
+        // printarray(array, length);
+    }
+}
+
+/* sort an array of integers by shell sort. */
+void shell_sort(int array[], int length)
+{
+    int i, j, gap;
+    for (gap = length / 2; gap > 0; gap /= 2)
+    {
+        for (j = gap; j < length; j++)
+        {
+            for (i = j; i - gap >= 0 && array[i - gap] > array[i]; i -= gap)
+            {
+                swap(&array[i], &array[i - gap]);
+            }
+        }
+        // printarray(array, length);
+    }
+}
+
+/* Split an array into two subsets. */
+int partition(int array[], int from, int to)
+{
+    int j;
+    int pos = from;
+    int pivot = array[to];
+    for (j = from; j < to; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            swap(&array[pos], &array[j]);
+            pos++;
+        }
+    }
+    swap(&array[pos], &array[to]);
+    return pos;
+}
+
+/* sort an array of integers by quick sort. */
+void quick_sort(int array[], int from, int to, int length)
+{
+    int pi;
+    if (from < to)
+    {
+        pi = partition(array, from, to);
+        // printarray(array + from, pi - 1 - from + 1);
+        // printarray(array + pi + 1, to - pi - 1 + 1);
+        quick_sort(array, from, pi - 1, length);
+        quick_sort(array, pi + 1, to, length);
+    }
+}
+
+/* Build up the heap tree in array. */
+void heapify(int array[], int root, int length) 
+{ 
+    int largest = root; // Initialize largest as root 
+    int left = 2 * root + 1; // left = 2*i + 1 
+    int right = 2 * root + 2; // right = 2*i + 2 
+  
+    if (left < length && array[left] > array[largest]) 
+        largest = left; 
+  
+    if (right < length && array[right] > array[largest]) 
+        largest = right; 
+  
+    if (largest != root)
+    { 
+        swap(&array[root], &array[largest]); 
+        heapify(array, largest, length); 
+    } 
+}
+
+/* sort an array of integers by heap sort. */
+void heap_sort(int array[], int length) 
+{ 
+    int i;
+    for (i = length / 2 - 1; i >= 0; i--)
+    {
+        heapify(array, i, length); 
+        // printarray(array, length);
+    }
+  
+    for (i = length - 1; i >= 0; i--) 
+    { 
+        swap(&array[0], &array[i]);
+        // printarray(array, length);
+        heapify(array, 0, i);
+    } 
+} 
+
+/* Merge the two subarrays for merge sort. */
+void merge(int array[], int start, int mid, int end)
+{
+    int i, j, k;
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
+    int left[n1], right[n2];
+
+    for (i = 0; i < n1; i++)
+        left[i] = array[start + i];
+    for (j = 0; j < n2; j++)
+        right[j] = array[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = start;
+
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
+        {
+            array[k] = left[i];
+            i++;
+        }
+        else
+        {
+            array[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        array[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        array[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+/* sort an array of integers by merge sort. */
+void merge_sort(int array[], int start, int end, int length)
+{
+    int mid;
+    if (start < end)
+    {  
+        mid = start + (end - start) / 2;
+        // printarray(array + start, mid - start + 1);
+        merge_sort(array, start, mid, length);
+        // printarray(array + start, mid - start + 1);
+        // printarray(array + mid + 1, end - mid - 1 + 1);
+        merge_sort(array, mid + 1, end, length);
+        // printarray(array + mid + 1, end - mid - 1 + 1);
+        merge(array, start, mid, end);
+    }
+}
+
+/* Copy an array of integers to another array of integers. */ 
+void duplicate(int array[], int targetarray[], int length)
+{
+    int i = 0;
+    for (i = 0; i < length; i++)
+    {
+        targetarray[i] = array[i];
     }
 }
 
 
-void quicksort(int array[], int length)
+int main(int argc, char const *argv[])
 {
-    int value = array[length - 1];
-    int i = 0, j = length - 2;
-    int idx = i;
-    printf("Length: %d\n", length);
-    printarray(array, length);
+    // To store the input data
+    int cnt = 0, choice;
+    int input[MAXSIZE] = {0};
+    int test1[MAXSIZE], test2[MAXSIZE];
 
-    if (length == 1)
-    {
-        return;
-    }
+    // For file I/O
+    int i, isnum;
+    FILE *fin, *fout;
+    char *data = NULL, *startp;
+    size_t len;
+	ssize_t bytes;
 
-    for (; i <= j; i++)
+    // Read in
+    fout = fopen(argv[2],"w+");
+    fin = fopen(argv[1], "r");
+    bytes = getline(&data, &len, fin);
+    data[bytes] = '\0';
+    fprintf(fout, "Input: %s\n", data);
+
+    // Parse the input and build a tree.
+    isnum = FALSE;
+    startp = NULL;
+    for (i = 0; i < bytes; i++)
     {
-        printf("array[%d]: %d\n", i, array[i]);
-        if (array[i] > value)
+        if (data[i] >= '0' && data[i] <= '9')
         {
-            for (; j > i; j--)
+            if (isnum == FALSE)
             {
-                if (array[j] < value)
-                {
-                    printf("Swap %d to %d\n", array[i], array[j]);
-                    swap(&array[i], &array[j]);
-                    idx = i;
-                    break;
-                }
+                startp = &(data[i]);
+                isnum = TRUE;
             }
         }
         else
         {
-            idx = i;
+            if (isnum == TRUE)
+            {
+                data[i] = '\0';
+                input[cnt++] = atoi(startp);
+                isnum = FALSE;
+            }
         }
     }
-    if (array[idx] < value)
+    if (isnum == TRUE)
     {
-        idx++;
+        input[cnt++] = atoi(startp);
+        isnum = FALSE;
     }
-    printarray(array, length);
-    printf("idx: %d\n", idx);
-    printarray(array, length);
-    quicksort(&array[0], idx);
-    swap(&array[idx], &array[length - 1]);
-    getchar();
-    quicksort(&array[idx + 1], (length - 1) - (idx + 1) + 1);
-    getchar();
-}
+    free(data);
+	fclose(fin);
 
+    // Prompt user to select node
+    duplicate(input, test1, cnt);
+    duplicate(input, test2, cnt);
 
+    while (1)
+    {
+        printf("The list of simple sorting methods:\n");
+        printf("1) Bubble sort.\n");
+        printf("2) Selection sort.\n");
+        printf("3) Shell sort.\n");
+        printf("4) Insertion sort.\n");
+        fprintf(fout, "The list of simple sorting methods:\n");
+        fprintf(fout, "1) Bubble sort.\n");
+        fprintf(fout, "2) Selection sort.\n");
+        fprintf(fout, "3) Shell sort.\n");
+        fprintf(fout, "4) Insertion sort.\n");
+        printf("Select a method to sort: ");
+        scanf("%d", &choice);
+        fprintf(fout, "Select a method to sort: %d\n", choice);
+        if (1 <= choice && choice <= 4)
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid. Try again.\n");
+            fprintf(fout, "Invalid. Try again.\n");
+        }
+    }
+    fprintf(fout, "The array before sorting: ");
+    printarray(test1, cnt, fout);
+    if (choice == 1)
+    {
+        bubble_sort(test1, cnt);
+    }
+    else if (choice == 2)
+    {
+        selection_sort(test1, cnt);
+    }
+    else if (choice == 3)
+    {
+        shell_sort(test1, cnt);
+    }
+    else if (choice == 4)
+    {
+        insertion_sort(test1, cnt);
+    }
+    fprintf(fout, "The array after sorting: ");
+    printarray(test1, cnt, fout);
 
-int main()
-{
-    int input[] = {49,17,56,85,12,97,33,71,19,62,38,84,51,29,77,65,44,99,4,47,67,41,23,88,73,8,100,25,91,58,59,22,15,35,95,60,20,7,50,10};
-    printarray(input, 40);
-    // bubblesort(input, 40);
-    // insertsort(input, 40);
-    // selectionsort(input, 40);
-    quicksort(input, 40);
-    printarray(input, 40);
+    while (1)
+    {
+        printf("The list of advanced sorting methods:\n");
+        printf("1) Quick sort.\n");
+        printf("2) Heap sort.\n");
+        printf("3) Merge sort.\n");
+        fprintf(fout, "The list of advanced sorting methods:\n");
+        fprintf(fout, "1) Quick sort.\n");
+        fprintf(fout, "2) Heap sort.\n");
+        fprintf(fout, "3) Merge sort.\n");
+        printf("Select a method to sort: ");
+        scanf("%d", &choice);
+        fprintf(fout, "Select a method to sort: %d\n", choice);
+        if (1 <= choice && choice <= 3)
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid. Try again.\n");
+            fprintf(fout, "Invalid. Try again.\n");
+        }
+    }
+    fprintf(fout, "The array before sorting: ");
+    printarray(test2, cnt, fout);
+    if (choice == 1)
+    {
+        quick_sort(test2, 0, cnt - 1, cnt);
+    }
+    else if (choice == 2)
+    {
+        heap_sort(test2, cnt);
+    }   
+    else if (choice == 3)
+    {
+        merge_sort(test2, 0, cnt - 1, cnt);
+    }   
+    fprintf(fout, "The array after sorting: ");
+    printarray(test2, cnt, fout);
+
+    // Cleanup
+    fclose(fout);
     return 0;
 }
 
-
-
-
-
-
-
-
-// struct node {
-//     int info;
-//     struct node *next;
-// };
-// typedef struct node *NODEPTR;
-
-// NODEPTR getnode(void)
-// {
-//     NODEPTR p;
-//     p = (NODEPTR) malloc(sizeof(struct node));
-//     return (p);
-// }
-
-// void freenode(NODEPTR p)
-// {
-//     free(p);
-// }
-
-// /* Place x in the tail of pstack */
-// void insafter(NODEPTR p, int x)
-// {
-//     NODEPTR q;
-//     if (p == NULL)
-//     {
-//         printf("void insertion \n");
-//         exit(1);
-//     }
-//     q = getnode();
-//     q->info = x;
-//     q->next = p->next;
-//     p->next = q;
-//     return;
-// }
-
-// void delafter(NODEPTR p, int *px)
-// {
-//     NODEPTR q;
-//     if ((p == NULL) || (p->next == NULL))
-//     {
-//         printf("void deletion \n");
-//         exit(1);
-//     }
-//     q = p->next;
-//     *px = q->info;
-//     p->next = q->next;
-//     freenode(q);
-//     return;
-// }
-
-// void place(NODEPTR *plist, int x)
-// {
-//     NODEPTR p, q = NULL;
-//     for (p = *plist; p != NULL && x > p->info; p = p->next)
-//     {
-//         q = p;
-//     }
-//     if (q == NULL)
-//     {
-//         push(plist, x);
-//     }
-//     else
-//     {
-//         insafter(q, x);
-//     }
-//     return;
-// }
-
-// NODEPTR search(NODEPTR list, int x)
-// {
-//     NODEPTR p;
-//     for (p = list; p != NULL; p = p->next)
-//     {
-//         if (p->info == x)
-//         {
-//             return (p);
-//         }
-//     }
-//     return (NULL);
-// }
-
-// void remvx(NODEPTR *plist, int x)
-// {
-//     NODEPTR p = *plist, q = NULL;
-//     int y;
-//     while (p != NULL)
-//     {
-//         if (p->info == x)
-//         {
-//             p = p->next;
-//             if (q == NULL)
-//             {
-//                 freenode(*plist);
-//                 *plist = p;
-//             }
-//             else
-//             {
-//                 delafter(q, &y);
-//             }
-//         }
-//         else
-//         {
-//             q = p;
-//             p = p->next;
-//         }
-//     }
-//     return;
-// }
-
-// /* Place x in the tail of pstack */
-// void push(NODEPTR *pstack, int x)
-// {
-//     NODEPTR p;
-//     p = getnode();
-//     p->info = x;
-//     if (empty(pstack) == TRUE)
-//     {
-//         *pstack = p;
-//     }
-//     else
-//     {
-//         p->next = (*pstack)->next;
-//     }
-//     (*pstack)->next = p;
-// }
-
-// int empty(NODEPTR *pstack)
-// {
-//     return (*pstack == NULL ? TRUE : FALSE);
-// }
-
-// /* Remove x in the tail of pstack */
-// int pop(NODEPTR *pstack)
-// {
-//     int x;
-//     NODEPTR p;
-//     if (empty(pstack) == TRUE)
-//     {
-//         printf("stack underflow \n");
-//         exit(1);
-//     }
-//     p = (*pstack)->next;
-//     x = p->info;
-//     if (p == *pstack)
-//     {
-//         *pstack = NULL;
-//     }
-//     else
-//     {
-//         *pstack = p->next;
-//     }
-//     freenode(p);
-//     return (x);
-// }
-
-// void insert(NODEPTR *pq, int x)
-// {
-//     NODEPTR p;
-//     p = getnode();
-//     p->info = x;
-//     if (empty(pq) == TRUE)
-//     {
-//         *pq = p;
-//     }
-//     else
-//     {
-//         p->next = (*pq)->next;
-//     }   
-//     p->next = (*pq)->next;
-//     *pq = p;
-//     return;
-// }
-
-// int remove(NODEPTR *pq)
-// {
-//     int x;
-//     NODEPTR p;
-//     if (empty(pq) == TRUE)
-//     {
-//         printf("queue underflow \n");
-//         exit(1);
-//     }
-//     p = (*pq)->next;
-//     x = p->info;
-//     if (p == *pq)
-//     {
-//         *pq = NULL;
-//     }
-//     else
-//     {
-//         *pq = p->next;
-//     }
-//     freenode(p);
-//     return (x);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// struct dlnode {
-//     int info;
-//     struct node *next;
-//     struct node *prev;
-// };
-// typedef struct dlnode *DLNDPTR, DLNODE;
-// DLNDPTR dllheader;
-
-
-
-// DLNDPTR makeDLNode(int dllistinfo)
-// {
-//     DLNDPTR pdln = getnode();
-//     pdln->info = dllistinfo;
-//     pdln->next = NULL;
-//     pdln->prev = NULL;
-//     return (pdln);
-// }
-
-// dllheader->next = dllheader;
-// dllheader->prev = dllheader;
-
-// void insertdlnatEnd(DLNDPTR dllheader, DLNDPTR pdln)
-// {
-//     pdln->prev = dllheader->prev;
-//     dllheader->prev->next = pdln;
-//     dllheader->prev = pdln;
-//     pdln->next = dllheader;
-//     return;
-// }
-
-
-// int deleteatpdln(DLNDPTR dllheader, DLNDPTR pdln)
-// {
-//     int pdlninfo;
-//     if (pdln && dllheader)
-//     {
-//         pdln->info = pdln->info;
-//         pdln->prev->next = pdln->next;
-//         pdln->next->prev = pdln->prev;
-//         pdln->prev = NULL;
-//         pdln->next = NULL;
-//         free(pdln);
-//         return (pdlninfo);
-//     }
-//     return (-1);
-// }
-
-
-
-
-
-
-
-
-
-// int main(int argc, char const *argv[])
-// {
-//     char cmd;
-
-//     // For file I/O
-//     FILE *fout;
-//     fout = fopen ("output.txt","w+");
-
-//     do
-//     {
-//         printf("Place your queen in a row [0-7, Q]:");
-//         scanf(" %c", &cmd);
-//         if (cmd != 'Q' || (cmd < '0' || cmd > '7'))
-//         {
-//             printf("Invalid. Please try again.\n");
-//         }
-//         else
-//         {
-//             if (cmd == 'Q')
-//                 break;
-//             printf("Invalid. Please try again.\n");
-//         }
-//     }
-//     while (TRUE);
-
-//     fclose(fout);
-   
-//     return 0;
-// }
